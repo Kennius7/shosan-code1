42f5790480b5f91632a49ce37fc52b6887a34a35
Kenniusboggs <ogbogukenny@yahoo.com>
2022-02-25 23:31:00 +0100

    Work done on the 25th of February 2022 at Alabians

---
+++ javascriptclass/jsday4/object.js
@@ -0,0 +1,40 @@
+// const Person = {
+//     firstName: "Kenny",
+//     lastName: "Ogbogu",
+//     Age: "20",
+//     Country: "Nigeria",
+//     skinColor: "Fair",
+//     Walk: function trek(dist) {
+//     }
+// }
+
+// alert(`My name is ${Person.firstName}`)
+// alert(`My country is ${Person.Country} though I go soon comot for here.`)
+// // alert(`You will walk ${Person.Walk} kilometers`)
+
+// alert(Person.reproduce = "Twins");
+// delete Person.skinColor
+
+
+
+
+// console.log(`My name is ${Person.firstName}`)
+// console.log(`My country is ${Person.Country} though I go soon comot for here.`)
+// // alert(`You will walk ${Person.Walk} kilometers`)
+
+// console.log(Person.reproduce = "Twins");
+// delete Person.skinColor
+
+
+const Questionnaire = [
+    {questionOne: "Who is the best player in the world?"},
+    {questionTwo: "Who is the best player in the world?"},
+    {questionThree: "Who is the best player in the world?"}
+]
+
+// alert(Questionnaire[1].questionTwo);
+
+for (let i = 0; i <= Questionnaire.length; i++){
+  alert(Questionnaire[i]);
+  let 
+}
