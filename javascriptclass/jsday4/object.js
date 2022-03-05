8f65a25c6dd0a54c9f68f617d359224eecb4b24c
Kenniusboggs <ogbogukenny@yahoo.com>
2022-03-05 10:30:59 +0100

    Back up purposes

--- javascriptclass/jsday4/object.js
+++ javascriptclass/jsday4/object.js
@@ -26,15 +26,167 @@
 // delete Person.skinColor
 
 
-const Questionnaire = [
-    {questionOne: "Who is the best player in the world?"},
-    {questionTwo: "Who is the best player in the world?"},
-    {questionThree: "Who is the best player in the world?"}
-]
+// const Questionnaire = [
+//     {questionOne: "Who is the best player in the world?"},
+//     {questionTwo: "Who is the best player in the world?"},
+//     {questionThree: "Who is the best player in the world?"}
+// ]
 
-// alert(Questionnaire[1].questionTwo);
+// // alert(Questionnaire[1].questionTwo);
 
-for (let i = 0; i <= Questionnaire.length; i++){
-  alert(Questionnaire[i]);
-  let 
-}
+// for (let i = 0; i <= Questionnaire.length; i++){
+//   alert(Questionnaire[i]);
+//   let 
+// }
+
+// document.write("Hello World")
+
+// let number = 5; number <= 10; number++;
+
+// alert(number);
+// alert(typeof (number));
+
+// let data = prompt();
+// alert(`The data type is ${typeof data}`);
+
+// let ogboguFamilyMembers = ["kenny", "Karle", "Erica", "Rayen"];
+// ogboguFamilyMembers = [1000, 500, 200, 100];
+// alert(ogboguFamilyMembers[1]);
+
+// let str = "shosan";
+// let char = [...str];
+// alert(char);
+
+// let arr1 = [1, 2, 3];
+// let arr2 = [-3, -2, -1, 0, ...arr1];
+// alert(arr2);
+
+// function add(a,b,c) {
+//     return a + b + c;
+// }
+
+// let values = [2, 3, 4];
+// // let sum = add(values[0], values[1], values[2]);
+// sum = add(...values);
+// alert(sum);
+
+// let arr3 = [3, 4, 5, 6, 7, 8];
+// arr3.pop();
+// arr3.push(100);
+// arr3.shift();
+// arr3.unshift(2);
+// alert(arr3);
+// alert(arr3[arr3.length - 1]);
+
+
+// let age = prompt("How old are you?");
+// age <= 17
+//     ?alert("You aren't old enough")
+//     :alert("Welcome in");
+
+
+// document.write("Multiplication Tables <br>");
+// for (let x = 2; x <= 12; x++) {
+//     for (let y = 1; y <=12; y++){
+//         let result = x * y;
+//         document.write(`${x} x ${y} = ${result}<br>`);
+// }
+//     document.write("<br>");
+  
+// }
+
+// for (let length = 1; length <= 5; length++) {
+//     for (let breadth = 1; breadth <= 5; breadth++){
+//         let result = length + breadth;
+//         alert(`The length of the building, which is ${length} plus the breadth which is ${breadth} is equals to ${result}`)
+//     }
+// }
+
+
+// let itemCollection = [
+//                         ["Honda", "Hyundai", "Volkwagen", "Mitsubishi"],
+//                         ["Lagos", "Osun", "Ondo", "Benin"],
+//                         ["Samsung", "Itel", "Infinix", "Tecno"],
+//                         ["Kenny", "Karle", "Erica", "rayen"]
+//                     ]
+
+// for (let i = 0; i < itemCollection.length; i++) {
+//     let itemList = itemCollection[i];
+//     for (let x = 0; x < itemList.length; x++) {
+//         let items = itemList[x];
+//         alert(items);
+//     }
+// }
+
+// function addCalc(n1, n2) {
+//     let result = n1 + n2;
+//     alert(result);
+// }
+
+// addCalc(5, 9);
+
+
+// function discount(price, discAmount = 10) {
+//     let disPrice = price * (100 - discAmount) / 100;
+//     alert(`Your new price is ${disPrice}`);
+// }
+// discount(prompt("How much is the item?"));
+
+
+// class Person {
+//     constructor (name, dob){
+//         this.name = name;
+//         this.dob = dob;
+//     }
+//     sayName() {
+//         return(`Hi. My name is ${this.name} and I was born on ${this.dob}`);
+//     }
+// }
+// let Kenny = new Person("Kenny Ogbogu", "November 5th");
+// alert(Kenny.sayName());
+
+
+// const Person = {
+//     name: "Kenny", surname: "Ogbogu", age: 32, hobby: ["reading", "gaming", "drawing"],
+//     address: "12 Rosamond street, Lagos", height: 6.4, 
+//     car: {
+//         name: "Toyota", price: "N2,000,000"
+//     },
+
+//     walk: () => alert(`${Person.name} ${Person.surname}, who is aged ${Person.age} years, 
+//     and has a height of ${Person.height} feet, has a number of hobbies like 
+//     ${Person.hobby} and lives at ${Person.address}. 
+//     He bought a ${Person.car.name} car for ${Person.car.price} from ${Person.bank}`),
+
+//     logHello: function(){
+//         alert("Hello World")
+//     }
+// }
+
+// Person.age = 40;
+// Person.bank = "GTB"
+
+// Person.walk();
+
+// Person.logHello();
+
+// const today = new Date();
+// alert(today);
+
+// getDate();
+// alert(getDate);
+
+// function logHello() {
+//     alert("Hello World")
+// }
+
+// logHello();
+
+// logHello = () => alert("Hello World");
+
+// logHello();
+
+
+
+
+
